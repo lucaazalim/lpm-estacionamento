522b02313a71c91ccd0769c106f40445f6dab4bd
Guilherme Leroy <112895265+ggleroy@users.noreply.github.com>
2023-10-04 12:19:42 -0300

    Feito construtor e método addVeiculo

--- codigo/src/main/java/br/pucminas/titas/Cliente.java
+++ codigo/src/main/java/br/pucminas/titas/Cliente.java
@@ -5,13 +5,20 @@ public class Cliente {
 	private String nome;
 	private String id;
 	private Veiculo[] veiculos;
+	private int contadorVeiculos = 0;
 
 	public Cliente(String nome, String id) {
-		
+		this.nome = nome;
+		this.id = id;
 	}
 
 	public void addVeiculo(Veiculo veiculo) {
-		
+		if (contadorVeiculos < veiculos.length) {
+			veiculos[contadorVeiculos] = veiculo;
+			contadorVeiculos++;
+		}else {
+			System.out.println("O vetor veiculos está cheio.");
+		}
 	}
 
 	public Veiculo possuiVeiculo(String placa) {
