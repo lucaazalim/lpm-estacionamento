6b3f2e2ae3146fc7b59e06699dfa0dac07890a49
AnaCarolinaMello <caldasdemelloanacarolina@gmail.com>
2023-10-04 11:28:16 -0300

    feat: add Vaga class methods

--- codigo/src/main/java/br/pucminas/titas/Vaga.java
+++ codigo/src/main/java/br/pucminas/titas/Vaga.java
@@ -6,19 +6,20 @@ public class Vaga {
 	private boolean disponivel;
 
 	public Vaga(int fila, int numero) {
-		
+		this.id = String.valueOf(fila) + String.format("%02d", String.valueOf(numero));
+		this.disponivel = true;
 	}
 
 	public boolean estacionar() {
-		
+		disponivel = false;
 	}
 
 	public boolean sair() {
-		
+		disponivel = true;
 	}
 
 	public boolean disponivel() {
-		
+		return disponivel;
 	}
 
 }
