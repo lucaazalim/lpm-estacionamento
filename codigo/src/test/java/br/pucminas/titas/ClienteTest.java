80caeedf432882923bd68ce7fb7f36c064223c61
Guilherme Leroy <112895265+ggleroy@users.noreply.github.com>
2023-10-11 11:43:37 -0300

    Correção descrição dos testes de Cliente

--- codigo/src/test/java/br/pucminas/titas/ClienteTest.java
+++ codigo/src/test/java/br/pucminas/titas/ClienteTest.java
@@ -28,35 +28,35 @@ public class ClienteTest {
     @Test
     public void testAddVeiculo() {
         cliente.addVeiculo(veiculo1);
-        assertNotNull(cliente.possuiVeiculo("ABC-1234")); //confirma que cliente pode adicionar veiculos
+        assertNotNull(cliente.possuiVeiculo("ABC-1234"),"confirma que cliente pode adicionar veiculos");
     }
 
     @Test
     public void testPossuiVeiculo() {
         cliente.addVeiculo(veiculo1);
-        assertNotNull(cliente.possuiVeiculo("ABC-1234")); //confirma que cliente possui veiculo1
-        assertNull(cliente.possuiVeiculo("PUZ-5654")); //confirma que cliente nao possui veiculo2
+        assertNotNull(cliente.possuiVeiculo("ABC-1234"),"confirma que cliente possui veiculo1");
+        assertNull(cliente.possuiVeiculo("PUZ-5654"),"confirma que cliente nao possui veiculo2");
     }
 
     @Test
     public void testTotalDeUsos() {
         cliente.addVeiculo(veiculo1);
         cliente.addVeiculo(veiculo2);
-        assertEquals(15, cliente.totalDeUsos()); //confirma total de usos dos veiculos
+        assertEquals(15, cliente.totalDeUsos(),"confirma total de usos dos veiculos");
     }
 
     @Test
     public void testArrecadadoPorVeiculo() {
         cliente.addVeiculo(veiculo1);
-        assertEquals(100.0, cliente.arrecadadoPorVeiculo("ABC-1234")); //confirma arrecadado veiculo1
-        assertEquals(0.0, cliente.arrecadadoPorVeiculo("PUZ-5654")); //confirma que veiculo2 nao teve arrecadacao
+        assertEquals(100.0, cliente.arrecadadoPorVeiculo("ABC-1234"), "confirma arrecadado veiculo1");
+        assertEquals(0.0, cliente.arrecadadoPorVeiculo("PUZ-5654"), "confirma que veiculo2 nao teve arrecadacao");
     }
 
     @Test
     public void testArrecadadoTotal() {
         cliente.addVeiculo(veiculo1);
         cliente.addVeiculo(veiculo2);
-        assertEquals(300.0, cliente.arrecadadoTotal()); //confirma o arrecadado dos dois veiculos
+        assertEquals(300.0, cliente.arrecadadoTotal(),"confirma o arrecadado dos dois veiculos");
     }
 
 }
