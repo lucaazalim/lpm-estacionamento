c79f8442d86c756034eb058193ddb8b87b3de57d
Guilherme Leroy <112895265+ggleroy@users.noreply.github.com>
2023-10-10 23:10:42 -0300

    documentando a Classe Cliente

--- codigo/src/main/java/br/pucminas/titas/Cliente.java
+++ codigo/src/main/java/br/pucminas/titas/Cliente.java
@@ -2,70 +2,108 @@ package br.pucminas.titas;
 
 public class Cliente {
 
-	private String nome;
-	private String id;
-	private Veiculo[] veiculos;
-	private int contadorVeiculos = 0;
+    private String nome;
+    private String id;
+    private Veiculo[] veiculos;
+    private int contadorVeiculos = 0;
 
-	public Cliente(String nome, String id) {
-		this.nome = nome;
-		this.id = id;
-		this.veiculos = new Veiculo[100]; //limite de 100 veículos
-	}
+    /**
+     * Constrói um novo objeto Cliente com o nome e id fornecidos.
+     * Inicializa um array vazio de veículos com um tamanho de 100.
+     *
+     * @param nome O nome do cliente.
+     * @param id O identificador único do cliente.
+     */
+    public Cliente(String nome, String id) {
+        this.nome = nome;
+        this.id = id;
+        this.veiculos = new Veiculo[100]; // limite de 100 veículos
+    }
 
-	public void addVeiculo(Veiculo veiculo) {
-		if (contadorVeiculos < veiculos.length) {
-			veiculos[contadorVeiculos] = veiculo;
-			contadorVeiculos++;
-		}
-	}
+    /**
+     * Adiciona um veículo à coleção do cliente.
+     *
+     * @param veiculo O veículo a ser adicionado.
+     */
+    public void addVeiculo(Veiculo veiculo) {
+        if (contadorVeiculos < veiculos.length) {
+            veiculos[contadorVeiculos] = veiculo;
+            contadorVeiculos++;
+        }
+    }
 
-	public Veiculo possuiVeiculo(String placa) {
-		for (Veiculo veiculo : veiculos) {
-			if (veiculo != null && veiculo.getPlaca().equals(placa)) { //classe veiculo implementar getPlaca
-				return veiculo;
-			}
-		}
-		return null;
-	}
+    /**
+     * Verifica se o cliente possui um veículo com uma determinada placa.
+     *
+     * @param placa A placa para verificar.
+     * @return O veículo com a placa correspondente, ou null se não encontrado.
+     */
+    public Veiculo possuiVeiculo(String placa) {
+        for (Veiculo veiculo : veiculos) {
+            if (veiculo != null && veiculo.getPlaca().equals(placa)) {
+                return veiculo;
+            }
+        }
+        return null;
+    }
 
-	public int totalDeUsos() {
-		int total = 0;
-		for (Veiculo veiculo : veiculos) {
-			if (veiculo != null) {
-				total += veiculo.getUsoTotal(); //classe veiculo implementar getUsoTotal
-			}
-		}
-		return total;
-	}
+    /**
+     * Calcula o total de usos de todos os veículos do cliente.
+     *
+     * @return O total de usos de todos os veículos.
+     */
+    public int totalDeUsos() {
+        int total = 0;
+        for (Veiculo veiculo : veiculos) {
+            if (veiculo != null) {
+                total += veiculo.getUsoTotal();
+            }
+        }
+        return total;
+    }
 
-	public double arrecadadoPorVeiculo(String placa) {
-		Veiculo veiculo = possuiVeiculo(placa);
-		if (veiculo != null) {
-			return veiculo.getArrecadacao(); //classe veiculo implementar getArrecadacao
-		}
-		return 0;
-	}
+    /**
+     * Recupera o montante total arrecadado por um veículo específico.
+     *
+     * @param placa A placa do veículo.
+     * @return O montante total arrecadado pelo veículo, ou 0 se o veículo não for encontrado.
+     */
+    public double arrecadadoPorVeiculo(String placa) {
+        Veiculo veiculo = possuiVeiculo(placa);
+        if (veiculo != null) {
+            return veiculo.getArrecadacao();
+        }
+        return 0;
+    }
 
-	public double arrecadadoTotal() {
-		double total = 0;
-		for (Veiculo veiculo : veiculos) {
-			if (veiculo != null) {
-				total += veiculo.getArrecadacao();
-			}
-		}
-		return total;
-	}
+    /**
+     * Calcula o montante total arrecadado por todos os veículos do cliente.
+     *
+     * @return O montante total arrecadado por todos os veículos.
+     */
+    public double arrecadadoTotal() {
+        double total = 0;
+        for (Veiculo veiculo : veiculos) {
+            if (veiculo != null) {
+                total += veiculo.getArrecadacao();
+            }
+        }
+        return total;
+    }
 
-	public double arrecadadoNoMes(int mes) {
-		double total = 0;
-		for (Veiculo veiculo : veiculos) {
-			if (veiculo != null) {
-				total += veiculo.getArrecadacaoNoMes(mes); //classe veiculo implementar getArrecadacaoNoMes
-			}
-		}
-		return total;
-	}
-
-
+    /**
+     * Calcula o montante arrecadado por todos os veículos do cliente em um determinado mês.
+     *
+     * @param mes O mês a ser considerado.
+     * @return O montante total arrecadado no mês especificado.
+     */
+    public double arrecadadoNoMes(int mes) {
+        double total = 0;
+        for (Veiculo veiculo : veiculos) {
+            if (veiculo != null) {
+                total += veiculo.getArrecadacaoNoMes(mes);
+            }
+        }
+        return total;
+    }
 }
