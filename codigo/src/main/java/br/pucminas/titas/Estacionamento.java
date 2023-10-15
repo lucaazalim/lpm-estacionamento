0ff2d5fafc0cc4338fad27023649d60cc1dfbf54
Gabriel Henrique Silva Pereira <130708252+Gab-HSP@users.noreply.github.com>
2023-10-15 17:20:10 -0300

    Consertando estacionar, sair, totalArrecadado e arrecadacaoNoMes #3

--- codigo/src/main/java/br/pucminas/titas/Estacionamento.java
+++ codigo/src/main/java/br/pucminas/titas/Estacionamento.java
@@ -7,12 +7,15 @@ public class Estacionamento {
 	private Vaga[] vagas;
 	private int quantFileiras;
 	private int vagasPorFileira;
+	private Map<String, Vaga> veiculoVagaMap = new HashMap<>();
 
 	public Estacionamento(String nome, int fileiras, int vagasPorFila) {
-		this.nome = nome;
-		this.quantFileiras = fileiras;
-		this.vagasPorFileira = vagasPorFila;
-	}
+	        this.nome = nome;
+	        this.id = new Cliente[100];
+	        this.vagas = new Vaga[fileiras * vagasPorFila];
+	        this.quantFileiras = fileiras;
+	        this.vagasPorFileira = vagasPorFila;
+		}
 
 	public void addVeiculo(Veiculo veiculo, String idCli) {
 		
@@ -26,28 +29,44 @@ public class Estacionamento {
 		
 	}
 
-	public void estacionar(String placa) {
-		for(int i = 0; i<Vaga.lenght(); i++) {
-			if(Vaga[i] == "") {
-				Vaga[i] = placa;
-			}
-		}
+	public void estacionar(String placa) throws EstacionamentoLotadoException {
+		boolean vagaEncontrada = false;
+		for (Vaga vaga : vagas) {
+        		if (vaga != null && vaga.disponivel()) {
+            			vaga.estacionar();
+            			veiculoVagaMap.put(placa, vaga);
+				vagaEncontrada = true;
+            			break;
+        		}
+    		}
+		if (!vagaEncontrada) {
+        		throw new EstacionamentoLotadoException("Não há vagas disponíveis para estacionar o veículo com placa " + placa);
+    		}
+
 	}
 
 	public double sair(String placa) {
-		for(int i = 0; Vaga.lenght(); i++) {
-			iv(Vaga[i] == placa) {
-				Vaga[i] = "";
-			}
+		Vaga vaga = veiculoVagaMap.get(placa);
+    		if (vaga != null) {
+        		vaga.sair();
+        		veiculoVagaMap.remove(placa);
 		}
 	}
 
 	public double totalArrecadado() {
-		
+		double total = 0;
+        	for (Cliente cliente : id) {
+            		total += cliente.arrecadadoTotal();
+        	}
+        	return total;
 	}
 
 	public double arrecadacaoNoMes(int mes) {
-		
+	        double total = 0;
+        	for (Cliente cliente : id) {
+            		total += cliente.arrecadadoNoMes(mes);
+        	}
+        	return total;
 	}
 
 	public double valorMedioPorUso() {
