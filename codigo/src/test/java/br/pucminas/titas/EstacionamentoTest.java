082c23b4e35bea8f6a65ddda6fca1341a7aa9843
Guilherme Leroy <112895265+ggleroy@users.noreply.github.com>
2023-11-14 12:35:35 -0300

    Implementando métodos em EstacionamentoTest

--- codigo/src/test/java/br/pucminas/titas/EstacionamentoTest.java
+++ codigo/src/test/java/br/pucminas/titas/EstacionamentoTest.java
@@ -1,7 +1,45 @@
 package br.pucminas.titas;
 
+import br.pucminas.titas.entidades.Cliente;
+import br.pucminas.titas.entidades.Estacionamento;
+import br.pucminas.titas.entidades.Veiculo;
+import br.pucminas.titas.enums.Servico;
+import br.pucminas.titas.excecoes.EstacionamentoLotadoException;
+import br.pucminas.titas.excecoes.VeiculoJaEstacionadoException;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class EstacionamentoTest {
 
-    // Tarefa de Guilherme Leroy.
+    private Estacionamento estacionamento;
+
+    @BeforeEach
+    void setUp() {
+        estacionamento = new Estacionamento("Xulambs Parking", 10, 10);
+    }
+
+    @Test
+    void testCadastrarCliente() {
+        assertDoesNotThrow(() -> estacionamento.cadastrarCliente("Guilherme"),"Afere se cadastro de cliente não lança exceção.");
+    }
+
+    @Test
+    void testGetClientes() {
+        assertNotNull(estacionamento.getClientes(),"Afere se método não retorna nulo.");
+    }
+    @Test
+    void testEstacionar() throws EstacionamentoLotadoException, VeiculoJaEstacionadoException {
+        Cliente cliente = new Cliente(2, "Guilherme");
+        Veiculo veiculo = new Veiculo("PUZ5654", cliente);
+        assertDoesNotThrow(() -> estacionamento.estacionar(veiculo, Servico.LAVAGEM),"Afere se estacionar não lança exceção.");
+    }
+    @Test
+    void testGetVagas() {
+        assertNotNull(estacionamento.getVagas(),"Afere se método não retorna nulo.");
+    }
 
 }
